0e841f2cc7028a76e5ea14c86f4f00ab0dff61a5
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2019-06-01 17:53:20 +0300

    start for "with me"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import State.MovieDownloader;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Die World!");
+        MovieDownloader movieDownloader=new MovieDownloader();
+        HashMap<Integer,String> events=new HashMap<>();
+        events.put(0,"sss");
+        events.put(1,"sss");
+        events.put(2,"sss");
+        events.put(3,"sss");
+        events.put(4,"sss");
+        events.put(5,"sss");
+        events.put(6,"sss");
+        events.put(7,"sss");
+        events.put(8,"sss");
+        events.put(9,"sss");
+        events.put(10,"sss");
+        events.put(11,"sss");
+        events.put(12,"sss");
     }
 }
