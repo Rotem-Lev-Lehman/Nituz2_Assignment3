232c4c7e83bf95e97a6c47d1ae3d430e54f6d4fc
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-06-01 20:08:07 +0300

    Fun with threads in second check :)

--- src/State/Power/On/Download/DownloadStateSecondCheck.java
+++ src/State/Power/On/Download/DownloadStateSecondCheck.java
@@ -3,8 +3,14 @@ package State.Power.On.Download;
 import State.Power.On.PowerStateOnMachine;
 
 public class DownloadStateSecondCheck extends ADownloadState {
+    private Thread secondCheckThread;
+    private Object lock;
+    private volatile boolean needToCheck;
+
     public DownloadStateSecondCheck(PowerStateOnMachine powerStateOnMachine) {
         super(powerStateOnMachine);
+        lock = new Object();
+        secondCheckThread = null;
     }
 
     @Override
@@ -15,17 +21,44 @@ public class DownloadStateSecondCheck extends ADownloadState {
     @Override
     public void enterState() {
         super.enterState();
-        //how to do tm(4) -> need to fix
-        try {
-            Thread.sleep(4000);
-            if(powerStateOnMachine.getSpaceAvailable()>=powerStateOnMachine.getCurrentFileSize()){
-                powerStateOnMachine.setCurrentDownloadState(powerStateOnMachine.getDownloadStateDownload());
+
+        synchronized (lock) {
+            if (secondCheckThread != null && secondCheckThread.isAlive()) {
+                secondCheckThread.interrupt();
             }
-            else{
-                powerStateOnMachine.setCurrentDownloadState(powerStateOnMachine.getDownloadStateIdle());
+            needToCheck = true;
+            secondCheckThread = new Thread(new myThread());
+            secondCheckThread.start();
+        }
+    }
+
+    public void setNeedToCheck(boolean bool){
+        needToCheck = bool;
+        if(!needToCheck){
+            synchronized (lock) {
+                if (secondCheckThread != null && secondCheckThread.isAlive()) {
+                    secondCheckThread.interrupt();
+                }
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        }
+    }
+
+    private class myThread implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(4000);
+                if(needToCheck) {
+                    if (powerStateOnMachine.getSpaceAvailable() >= powerStateOnMachine.getCurrentFileSize()) {
+                        powerStateOnMachine.setCurrentDownloadState(powerStateOnMachine.getDownloadStateDownload());
+                    } else {
+                        powerStateOnMachine.setCurrentDownloadState(powerStateOnMachine.getDownloadStateIdle());
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
